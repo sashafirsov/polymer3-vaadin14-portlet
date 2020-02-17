d5d756bb365e3f91a92fb967c269b2606c434d86
suns <suns@firsov.net>
2020-02-16 20:11:57 -0800

    META-INF/resources/webcomponents added as Polymer component with polymer-cli

---
+++ modules/polymer-vaadin-portlet/src/main/resources/META-INF/resources/webcomponents/webcomponents-element.js
@@ -0,0 +1,32 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `webcomponents-element`
+ * Container of Polymer Elements and Vaadin web components pulled as lazy loaded dependencies.
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class WebcomponentsElement extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'webcomponents-element',
+      },
+    };
+  }
+}
+
+window.customElements.define('webcomponents-element', WebcomponentsElement);
