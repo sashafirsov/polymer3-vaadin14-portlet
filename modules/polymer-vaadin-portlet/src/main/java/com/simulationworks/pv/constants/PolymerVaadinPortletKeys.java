ef65c82d6303026e96e1f98131500f282c0090ef
suns <suns@firsov.net>
2020-02-16 19:56:43 -0800

    polymer-vaadin-portlet created as MVC portlet project.

---
+++ modules/polymer-vaadin-portlet/src/main/java/com/simulationworks/pv/constants/PolymerVaadinPortletKeys.java
@@ -0,0 +1,11 @@
+package com.simulationworks.pv.constants;
+
+/**
+ * @author suns
+ */
+public class PolymerVaadinPortletKeys {
+
+	public static final String POLYMERVAADIN =
+		"com_simulationworks_pv_PolymerVaadinPortlet";
+
+}
---
+++ modules/polymer-vaadin-portlet/src/main/java/com/simulationworks/pv/portlet/PolymerVaadinPortlet.java
@@ -0,0 +1,30 @@
+package com.simulationworks.pv.portlet;
+
+import com.simulationworks.pv.constants.PolymerVaadinPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author suns
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=PolymerVaadin",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PolymerVaadinPortletKeys.POLYMERVAADIN,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class PolymerVaadinPortlet extends MVCPortlet {
+}
